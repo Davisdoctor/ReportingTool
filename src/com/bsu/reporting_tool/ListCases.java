4adab7822df81fd78ae4a1b57bcab72eb27ece59
Mukama Martin <mukama.martin@gmail.com>
2015-03-03 12:40:33 +0300

    no message

--- src/com/bsu/reporting_tool/ListCases.java
+++ src/com/bsu/reporting_tool/ListCases.java
@@ -20,22 +20,22 @@ public class ListCases extends Activity {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list_cases);
-		dbManager = new MyDatabase(this);
-		dbManager.open();
-		casesList = (ListView)findViewById(R.id.case_list);
-		String columns[] = { "serialNumber","crime","incidenceOccurred","areaOccurred","crimeId" };
-		int to[]= new int[]{ R.id.serial_number,R.id.crime ,R.id.incidenceOccurred, R.id.area};
-		dbcursor = dbManager.fetchAllRecords("crime_reporting",BuildDB.crimeColumns);
-		if(dbcursor.moveToFirst()){
-			 startManagingCursor(dbcursor);
-			this.setTitle("List of Cases["+ dbcursor.getCount()+"]");
-			 adapter = new SimpleCursorAdapter(this, R.layout.case_list_details,   dbcursor, columns, to);
-			// dbcursor.deactivate();
-			 casesList.setAdapter(adapter);
-			 dbManager.close();
-	
-			// casesList.setOnItemClickListener(this);
-		}
+//		dbManager = new MyDatabase(this);
+//		dbManager.open();
+//		casesList = (ListView)findViewById(R.id.case_list);
+//		String columns[] = { "serialNumber","crime","incidenceOccurred","areaOccurred","crimeId" };
+//		int to[]= new int[]{ R.id.serial_number,R.id.crime ,R.id.incidenceOccurred, R.id.area};
+//		dbcursor = dbManager.fetchAllRecords("crime_reporting",BuildDB.crimeColumns);
+//		if(dbcursor.moveToFirst()){
+//			 startManagingCursor(dbcursor);
+//			this.setTitle("List of Cases["+ dbcursor.getCount()+"]");
+//			 adapter = new SimpleCursorAdapter(this, R.layout.case_list_details,   dbcursor, columns, to);
+//			// dbcursor.deactivate();
+//			 casesList.setAdapter(adapter);
+//			 dbManager.close();
+//	
+//			// casesList.setOnItemClickListener(this);
+//		}
 	}
 
 	@Override
